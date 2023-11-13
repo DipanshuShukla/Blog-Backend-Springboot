7c1de582e78a5785f1d7fe949072d3bdd05b5859
DipanshuShukla <shuklascientist5@gmail.com>
2023-11-13 16:25:01 +0530

    Update BlogController.java

--- src/main/java/com/codebunny/NordicRose/controller/BlogController.java
+++ src/main/java/com/codebunny/NordicRose/controller/BlogController.java
@@ -2,6 +2,7 @@ package com.codebunny.NordicRose.controller;
 
 import com.codebunny.NordicRose.dto.BlogDTO;
 import com.codebunny.NordicRose.dto.BlogPageDTO;
+import com.codebunny.NordicRose.exception.BlogNotFoundException;
 import com.codebunny.NordicRose.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,9 +24,16 @@ public class BlogController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BlogPageDTO> findById(@PathVariable Integer id){
-        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    public ResponseEntity<?> findById(@PathVariable Integer id) {
+        try {
+            BlogPageDTO blogPageDTO = service.findById(id);
+            return new ResponseEntity<>(blogPageDTO, HttpStatus.OK);
+        } catch (BlogNotFoundException e) {
+            // Handle the exception and return an error response
+            return new ResponseEntity<>("Blog not found for ID: " + id, HttpStatus.NOT_FOUND);
+        }
     }
+
 
     @GetMapping("/paged")
     List<BlogDTO> findPaged(@RequestParam(defaultValue = "-1") Integer excluded_id, @RequestParam(defaultValue = "10") Integer pageSize, @RequestParam(defaultValue = "1") Integer pageNo){
