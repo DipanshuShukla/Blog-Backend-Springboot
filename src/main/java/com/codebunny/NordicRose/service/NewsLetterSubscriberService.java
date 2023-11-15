38c2ecc687c432de62830e756698b0bf5e9a565f
DipanshuShukla <shuklascientist5@gmail.com>
2023-11-15 16:24:07 +0530

    Update NewsLetterSubscriberService.java

--- src/main/java/com/codebunny/NordicRose/service/NewsLetterSubscriberService.java
+++ src/main/java/com/codebunny/NordicRose/service/NewsLetterSubscriberService.java
@@ -2,17 +2,22 @@ package com.codebunny.NordicRose.service;
 
 import com.codebunny.NordicRose.dto.NewsLetterSubscriberDTO;
 import com.codebunny.NordicRose.entity.NewsLetterSubscriber;
+import com.codebunny.NordicRose.exception.DuplicateSubscriberException;
 import com.codebunny.NordicRose.repository.NewsLetterSubscriberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsLetterSubscriberService {
+
     @Autowired
     private NewsLetterSubscriberRepository repository;
 
-    public NewsLetterSubscriberDTO addNew(NewsLetterSubscriberDTO dto){
-        if(repository.existsById(dto.getEmail())) return new NewsLetterSubscriberDTO();
+    public NewsLetterSubscriberDTO addNew(NewsLetterSubscriberDTO dto) throws DuplicateSubscriberException {
+        if (repository.existsById(dto.getEmail())) {
+            throw new DuplicateSubscriberException("Subscriber with email " + dto.getEmail() + " already exists.");
+        }
+
         NewsLetterSubscriber entity = new NewsLetterSubscriber();
         entity.setEmail(dto.getEmail());
         return NewsLetterSubscriberDTO.fromEntity(repository.save(entity));
