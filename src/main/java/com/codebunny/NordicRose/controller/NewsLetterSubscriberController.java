07cfd2e4a9149af5f8a6e6927845268bfd081109
DipanshuShukla <shuklascientist5@gmail.com>
2023-11-23 00:34:22 +0530

    Update NewsLetterSubscriberController.java

--- src/main/java/com/codebunny/NordicRose/controller/NewsLetterSubscriberController.java
+++ src/main/java/com/codebunny/NordicRose/controller/NewsLetterSubscriberController.java
@@ -1,6 +1,7 @@
 package com.codebunny.NordicRose.controller;
 
 import com.codebunny.NordicRose.dto.NewsLetterSubscriberDTO;
+import com.codebunny.NordicRose.exception.DuplicateSubscriberException;
 import com.codebunny.NordicRose.service.NewsLetterSubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,14 @@ public class NewsLetterSubscriberController {
 
     @CrossOrigin
     @PostMapping("/newsletter")
-    private ResponseEntity<NewsLetterSubscriberDTO> addEmail(@RequestBody NewsLetterSubscriberDTO email){
-        return new ResponseEntity<>(service.addNew(email), HttpStatus.OK);
+    private ResponseEntity<?> addEmail(@RequestBody NewsLetterSubscriberDTO email){
+        try {
+            NewsLetterSubscriberDTO result = service.addNew(email);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (DuplicateSubscriberException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An unexpected error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
